08a96e1e17e58a87f0e5831ad42efc40ed6bb825
Victor <victor5055@outlook.com>
2022-09-19 09:00:24 -0400

    Adding Dom Elements.

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+//DOM elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboard = document.getElementById('clipboard');
+
+
+
+
+
+
+
+
 //--- Generator functions---// - https://www.net-comber.com/charset.html
 // Generate a random lower case letter
 

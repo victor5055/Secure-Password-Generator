3ec866e8665f3982ac97dfe30ee61f103e7eee2d
Victor <victor5055@outlook.com>
2022-09-18 23:59:43 -0400

    Adding Generator functions.

---
+++ script.js
@@ -0,0 +1,19 @@
+//--- Generator functions---// - https://www.net-comber.com/charset.html
+// Generate a random lower case letter
+
+function getRandomLower() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+	const symbols = '!@#$%^&*(){}[]=<>/,.'
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}

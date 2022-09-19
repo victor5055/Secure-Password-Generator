5d2dcff83b649a9f918e8ab229545b40597f3add
Victor <victor5055@outlook.com>
2022-09-19 09:26:50 -0400

    generate password function and loop.

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ clipboard.addEventListener('click', () => {
 	alert('Password copied to clipboard');
 });
 
-// Generate Event Listenr
+// Generate Event Listener
 generate.addEventListener('click', () => {
 	const length = +lengthEl.value;
 	const hasLower = lowercaseEl.checked;
@@ -41,6 +41,30 @@ generate.addEventListener('click', () => {
 	
 	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
 });
+
+//Generate password function
+function generatePassword(lower, upper, number, symbol, length) {
+	let generatedPassword = '';
+	const typesCount = lower + upper + number + symbol;
+	const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+	
+	// Doesn't have a selected type
+	if(typesCount === 0) {
+		return '';
+	}
+	
+	// create a loop
+	for(let i=0; i<length; i+=typesCount) {
+		typesArr.forEach(type => {
+			const funcName = Object.keys(type)[0];
+			generatedPassword += randomFunc[funcName]();
+		});
+	}
+	
+	const finalPassword = generatedPassword.slice(0, length);
+	
+	return finalPassword;
+}
 
 
 
